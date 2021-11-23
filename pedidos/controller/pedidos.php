b8fe8a5144f8219ddcf3a84b206850de9f0fbc7e
RodolfoBarahona <rnbarahona@unah.hn>
2021-11-23 15:46:52 -0600

    ...

--- pedidos/controller/pedidos.php
+++ pedidos/controller/pedidos.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
 
-    require_once("../config/conexion.php");
+    require_once("../../config/conexion.php");
     require_once("../pedidos/models/Pedidos.php");
     $pedidos = new Pedidos();
 
